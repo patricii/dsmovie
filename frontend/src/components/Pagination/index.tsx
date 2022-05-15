1573cc240164a1d6ae6587a1a8dd381e07f48654
thqv74 <thqv74@gmail.com>
2022-05-15 17:18:59 -0300

    Save, useNavigate

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -12,13 +12,11 @@ function Pagination({ page , onChange } : Props) {
     return (
         <div className="dsmovie-pagination-container">
             <div className="dsmovie-pagination-box">
-                <button className="dsmovie-pagination-button" 
-                disabled={page.first} onClick={() => onChange(page.number - 1)} >
+                <button className="dsmovie-pagination-button" disabled={page.first} onClick={() => onChange(page.number - 1)} >
                     <Arrow />
                 </button>
                 <p>{`${page.number + 1} de ${page.totalPages}`}</p>
-                <button className="dsmovie-pagination-button"
-                 disabled={page.last} onClick={() => onChange(page.number + 1)} >
+                <button className="dsmovie-pagination-button" disabled={page.last} onClick={() => onChange(page.number + 1)} >
                     <Arrow className="dsmovie-flip-horizontal" />
                 </button>
             </div>
